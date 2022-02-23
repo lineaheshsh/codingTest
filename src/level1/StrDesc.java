a90edee9743ee7ce534cdfe32646edd3d85e5186
LeeChangHo <lineaheshsh@naver.com>
2022-02-23 22:53:15 +0900

    [level1] 문자열 내림차순으로 배치하기 클리어 - 참고 코드 추가

--- src/level1/StrDesc.java
+++ src/level1/StrDesc.java
@@ -1,21 +1,30 @@
 package level1;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StrDesc {
     public static String solution(String s) {
-        String answer = "";
-        Character[] cArr = new Character[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            cArr[i] = s.charAt(i);
-        }
+//        String answer = "";
+//        Character[] cArr = new Character[s.length()];
+//        for (int i = 0; i < s.length(); i++) {
+//            cArr[i] = s.charAt(i);
+//        }
+//
+//        Arrays.sort(cArr, (c1, c2) -> c2 - c1);
+//
+//        for (int i = 0; i < cArr.length; i++) {
+//            answer += cArr[i];
+//        }
 
-        Arrays.sort(cArr, (c1, c2) -> c2 - c1);
-
-        for (int i = 0; i < cArr.length; i++) {
-            answer += cArr[i];
-        }
-        return answer;
+        /**
+         * 스트림 사용한 코드(참고용 ㅜㅜ)
+         */
+        return Stream.of(s.split(""))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining());
     }
 
     public static void main(String[] args) {
