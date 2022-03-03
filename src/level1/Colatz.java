8e2bebd7ad45d7c90015e58a0e77f41666c3cc66
LeeChangHo <lineaheshsh@naver.com>
2022-03-04 01:19:14 +0900

    [level1] 콜라츠 추측 클리어

--- src/level1/Colatz.java
+++ src/level1/Colatz.java
@@ -3,41 +3,36 @@ package level1;
 public class Colatz {
     public static int solution(int num) {
         int answer = 0;
+        long numToLong = (long) num;
         int cnt = 1;
 
-        if (num == 1) {
+        if (numToLong == 1) {
             return 1;
         } else {
-            answer = colatz(num, cnt);
+            while (cnt <= 500) {
+
+                if (numToLong % 2 == 0) {
+                    numToLong = numToLong / 2;
+                } else {
+                    numToLong = (numToLong * 3) + 1;
+                }
+
+                if (numToLong == 1) break;
+                cnt++;
+            }
         }
+
+        if (cnt >= 500) return -1;
+        else answer = cnt;
 
         return answer;
-    }
-
-    private static int colatz(int num, int cnt) {
-        if (cnt == 500) return -1;
-        else if (num == 1) return cnt;
-
-        if (num % 2 == 0) {
-            num = num / 2;
-        } else {
-            num = (num * 3) + 1;
-        }
-
-
-        System.out.println("1 : " + num + ", " + cnt);
-        if (num > 1) {
-            cnt++;
-            colatz(num, cnt);
-        }
-        return cnt;
     }
 
     public static void main(String[] args) {
         int[] n = {
                 6,
-//                16,
-//                626331
+                16,
+                626331
         };
 
         for (int i = 0; i < n.length; i++) {
