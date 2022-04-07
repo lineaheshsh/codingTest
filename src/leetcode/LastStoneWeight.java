2c31882523f232b51570640568370101931fa174
zzangho <lineaheshsh@naver.com>
2022-04-07 18:46:25 +0900

    [leetcode] 1046. Last Stone Weight 클리어 - 성능 개선

--- src/leetcode/LastStoneWeight.java
+++ src/leetcode/LastStoneWeight.java
@@ -1,52 +1,28 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class LastStoneWeight {
 
 
     public static int lastStoneWeight(int[] stones) {
-        List<Integer> list = Arrays.stream(stones).boxed().toList();
-        List<Integer> resultList = recursionFunc(list);
-        int r = 0;
-        if (resultList.size() > 0) {
-            r = resultList.get(0);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < stones.length; i++) {
+            priorityQueue.add(stones[i]);
         }
+
+        while (priorityQueue.size() > 1) {
+            System.out.println(priorityQueue.toString());
+            int y = priorityQueue.poll();
+            int x = priorityQueue.poll();
+            if (y > x) {
+                priorityQueue.add(y - x);
+            }
+        }
+
+        int r = priorityQueue.size() == 0 ? 0 : priorityQueue.poll();
+
         return r;
-    }
-
-    private static List<Integer> recursionFunc(List<Integer> stones) {
-        List<Integer> list = new ArrayList<>();
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        for (int i = 0; i < stones.size(); i++) {
-            priorityQueue.add(stones.get(i));
-        }
-
-        if (stones.size() <= 1) {
-            return stones;
-        } else {
-            int[] temp = new int[stones.size()];
-            int i = 0;
-            while (!priorityQueue.isEmpty()) {
-                temp[i++] = priorityQueue.poll();
-            }
-
-            for (int j = 0; j < temp.length-2; j++) {
-                list.add(temp[j]);
-            }
-
-            int result;
-            System.out.println("temp : " + Arrays.toString(temp));
-            if (temp[temp.length-1] > temp[temp.length-2]) {
-                result = temp[temp.length-1] - temp[temp.length-2];
-                list.add(result);
-            }
-
-            return recursionFunc(list);
-        }
     }
 
     public static void main(String[] args) {
