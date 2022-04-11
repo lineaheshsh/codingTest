a292b04c6c1d006d29e3de27c048de1dda5ae1cc
ZZangHO <lineaheshsh@naver.com>
2022-04-11 13:09:05 +0900

    Time: 4 ms (95.74%), Space: 42.8 MB (94.57%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,40 +1,21 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int total = m * n;
+        // if shifting total times, it shifts back to orignal state
+        k = k % (total);
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        int[] arr = new int[grid.length * grid[0].length];
-        int idx = 0;
-        int pos_j = grid[0].length;
-
-        for (int i = 0; i < grid.length; i++) {
-            int[] rows = grid[i];
-            for (int j = 0; j < rows.length; j++) {
-                arr[idx++] = rows[j];
+        for(int i = 0; i < m; i++) {
+            List<Integer> list = new ArrayList<>();
+            result.add(list);
+            for (int j = 0; j < n; j++) {   
+                // i * n + j original place index in 1D array
+                // i * n + j - k  is to get value k steps before                 
+                int index = ((i * n + j) - k + total) % total;
+                list.add(grid[index / n][index % n]);
             }
         }
-
-        for (int i = 0; i < k; i++) {
-            int lastNum = arr[arr.length - 1];
-            for (int j = arr.length - 2; j >= 0; j--) {
-                arr[j + 1] = arr[j];
-            }
-            arr[0] = lastNum;
-        }
-
-        idx = 0;
-        for (int i = 0; i < arr.length; i++) {
-
-            if (idx == pos_j) {
-                idx = 0;
-                result.add(list);
-                list = new ArrayList<>();
-            }
-            list.add(arr[i]);
-            idx++;
-
-            if (i+1 == arr.length) result.add(list);
-        }
-        
         return result;
     }
 }
