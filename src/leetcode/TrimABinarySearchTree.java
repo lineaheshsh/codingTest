786a7747f2d6962e2e4f2fb5ec1c5254917a5e10
zzangho <lineaheshsh@naver.com>
2022-04-21 21:01:32 +0900

    [leetcode] 669. Trim a Binary Search Tree 클리어 - 해설참조

---
+++ src/leetcode/TrimABinarySearchTree.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class TrimABinarySearchTree {
+
+    public static TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) return root;
+        if (root.val > high) return trimBST(root.left, low, high);
+        if (root.val < low) return trimBST(root.right, low, high);
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode1 = new TreeNode(0);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode = new TreeNode(1, treeNode1, treeNode2);
+
+        TreeNode resultNode = trimBST(treeNode, 1, 2);
+    }
+}
